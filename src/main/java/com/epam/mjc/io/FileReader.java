b1aaa411d0667b2bfecd93ca2f9e1632d4618fea
kart <mkartveli9@gmail.com>
2023-03-03 01:05:20 +0000

    3.1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
 
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String stri = "";
+        try (FileInputStream newfile = new FileInputStream(file)) {
+            int ch;
+
+            while ((ch = newfile.read()) != -1) {
+                stri += (char)ch;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Not fount");
+        } catch (IOException e) {
+            System.out.println("missing something");
+        }
+        String[] pairs = stri.split("\n");
+        System.out.println(pairs[1]);
+        String[] newpr = new String[pairs.length];
+        for (int i = 0; i < pairs.length; i++) {
+            newpr[i] = (pairs[i].split(": "))[1].trim();
+        }
+        String num = newpr[1];
+
+
+        return  new Profile(newpr[0], Integer.parseInt(newpr[1]), newpr[2], Long.parseLong(newpr[3]));
     }
 }
